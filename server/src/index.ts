207044a12f21e3ba927ebc0a9bfdc5a95de2b208
Alex <alex@swingai.com>
2026-03-26 22:53:07 -0400

    Add Railway config, dynamic PORT

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ import { hasDatabase, getPositions, createPosition, deletePosition, findPosition
 import { Position } from './types';
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3001;
 
 const allowedOrigins = process.env.CLIENT_URL
   ? [process.env.CLIENT_URL, 'http://localhost:3000']
